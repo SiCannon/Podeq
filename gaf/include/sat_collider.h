b302df4f0c88013a2bf7ece486c0d2148af2fbaa
SiCannon <simon.cannon@gmail.com>
2016-05-10 20:37:58 +0100

    More SATs

---
+++ gaf/include/sat_collider.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "polygon2f.h"
+
+class SatCollider
+{
+public:
+	bool collide(Polygon2f *poly1, Polygon2f *p2);
+};
---
+++ gaf/src/sat_collider.cpp
@@ -0,0 +1,40 @@
+#include "../include/sat_collider.h"
+#include "../include/polygon2f.h"
+#include "../include/vector2f.h"
+
+bool SatCollider::collide(Polygon2f *poly1, Polygon2f *poly2)
+{
+	bool collision = true;
+
+	for (short i = 0; i < poly1->vertex_count; i++)
+	{
+		short i1 = i;
+		short i2 = i + 1;
+		if (i2 >= poly1->vertex_count)
+		{
+			i2 = 0;
+		}
+
+		Vector2f a = poly1->vertices[i1];
+		Vector2f b = poly1->vertices[i2];
+		Vector2f n = normal(a, b);
+
+		bool all_negative = true;
+
+		for (short j = 0; j < poly2->vertex_count; j++)
+		{
+			glf dot = dot_product(minus(a, poly2->vertices[j]), n);
+			if (dot >= 0)
+			{
+				all_negative = false;
+				break;
+			}
+		}
+
+		if (all_negative)
+		{
+			collision = false;
+			break;
+		}
+	}
+}
