58ed8b39f6aaeda914cefa6f684b004cdc59335e
SiCannon <simon.cannon@gmail.com>
2016-05-14 09:39:33 +0100

    Build is fixed

--- gaf/include/gaf_math.h
+++ gaf/include/gaf_math.h
@@ -3,8 +3,6 @@
 #include <math.h>
 #include <GL/freeglut.h> 
 #include <include/gaf.h>
-#include <include/transform_old.h>
-#include <include/vector2f.h>
 
 #define PI 3.141592654f
 #define TWO_PI 6.283185307f
@@ -13,6 +11,9 @@
 #define PI_BY_8 0.392699082f
 
 #define SQR(x) ((x) * (x))
+
+struct Vector2f;
+class Transform_Old;
 
 GLfloat angleBetween(GLfloat ux, GLfloat uy, GLfloat vx, GLfloat vy);
 GLfloat r_to_d(GLfloat r);
